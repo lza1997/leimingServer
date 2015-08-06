d0a18a7be75b410ba4683e569d05ffbaf9c91202
springChun <535182324@qq.com>
2015-08-06 11:48:36 +0800

    返回titles的接口基本完成，同时设置了返回的编码

--- src/cn/zhang/service/impl/TitleServiceImpl.java
+++ src/cn/zhang/service/impl/TitleServiceImpl.java
@@ -23,10 +23,10 @@ public class TitleServiceImpl implements TitleService {
         List<Title> list=titleDao.getList(type);
         JSONObject json=new JSONObject();
         if (list.size()<=0) {
-            return json.put("state", 1);
+            return json.put("state", 3); //3代表没有对应类型的数据
         }
         json.put("titles", titleDao.getList(type));
-        return json.put("state", 0);
+        return json.put("state", 1); //1代表返回数据成功
     }
 
 }
